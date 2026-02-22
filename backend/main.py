fd0208d5c6c24208712ad314b68bc13fad13cba4
Johnny <johnnyflores@live.ca>
2026-02-21 21:15:52 -0500

    Implement upload images

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from routers import user, post
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -13,3 +14,5 @@ def root():
     return "Hello World"
 
 models.Base.metadata.create_all(engine)
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
--- backend/routers/post.py
+++ backend/routers/post.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, UploadFile, File
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
 from db import db_post
+import random
+import shutil
+import string
 from typing import List
 
 router = APIRouter(
@@ -25,3 +28,16 @@ def create(request: PostBase, db: Session = Depends(get_db)):
 @router.get('/all', response_model=List[PostDisplay])
 def posts(db: Session = Depends(get_db)):
     return db_post.get_all(db)
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+    letters = string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, 'w+b') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    
+    return {"filename": path}
