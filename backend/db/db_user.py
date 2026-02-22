743dda212c4763df050ab4e70220018091da3ab9
Johnny <johnnyflores@live.ca>
2026-02-21 20:15:00 -0500

    Add password encryption

--- backend/db/db_user.py
+++ backend/db/db_user.py
@@ -1,12 +1,13 @@
 from db.models import DbUser
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
+from db.hashing import Hash
 
 def create_user(db: Session, user: UserBase):
     new_user = DbUser(
         username = user.username,
         email = user.email,
-        password = user.password
+        password = Hash.bcrypt(user.password)
     )
     db.add(new_user)
     db.commit()
