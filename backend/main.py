dbf1537cf13a5232479f33b649e5c17065374051
Johnny <johnnyflores@live.ca>
2026-02-21 22:58:01 -0500

    Add CORS to connect the frontend side

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from routers import user, post, comment
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -14,7 +15,19 @@ app.include_router(authentication.router)
 
 @app.get("/")
 def root():
-    return "Hello World"
+    return {"Welcome": "Mike App Social Media"}
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
